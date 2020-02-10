e228ac85fc38073a86a0f8a343cb3a6d6563b393
Heinz K <heka@techcollege.dk>
2020-02-10 13:12:54 +0100

    ServiceWorker Update - WORKING!!!

--- public_html/service-worker.js
+++ public_html/service-worker.js
@@ -1,26 +1,52 @@
 'use strict';
 
-// Opdater cache navn når cachen ændres
-const CACHE_NAME = 'static-cache-v1';
+self.addEventListener("install", function(e) {
+    console.log("Service Worker Installed");
+    importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+    e.waitUntil(
+        caches.open('pwastartup').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
 
-// Liste over filer der skal caches
-const FILES_TO_CACHE = [
-    "index.html"
-];
+/**
+ * SW Activate
+ */
+self.addEventListener("activate", function(event) {
+});
 
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.open('pwastartup').then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                return response || fetch(event.request).then(function(response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+/*
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
-  //self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
   // CODELAB: Remove previous cached data from disk.
-
-  //self.clients.claim();
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
 
 });
+*/

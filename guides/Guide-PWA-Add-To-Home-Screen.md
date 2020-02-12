8286ff962bd0aa49fc0b31332a4dab31f4c39bd6
Heinz K <heka@techcollege.dk>
2020-02-12 14:25:01 +0100

    SCSS Updates

---
+++ guides/Guide-PWA-Add-To-Home-Screen.md
@@ -0,0 +1,56 @@
+# Guide til opsætning af tilføjelse af App til hjemmeskærm
+
+Følgende guide vil vise dig hvordan du kan opsætte din App's UI til at give brugerne mulighed for at tilføje app'en til deres hjemmeskærm.
+
+Guiden tager udgangspunkt i at du har gennemført Guide-PWA-Setup og at du har åbnet dette projekt i VS Code.
+
+## Målsætning
+
+Målet er at lavet et banner i vores UI som kan trigge browseren til at tilføje vores site til hjemmeskærm. Til at gøre dette skal vi bruge:
+* Et UI banner i HTML/CSS med en OK og en Annuller knap.
+* Et javascript der lytter på om appen allerede er installeret
+* Et javascript der kan håndtere installationen
+
+### iOS Mobiler
+Det følgende script virker på mobiler som kører Android men ikke på iphones.. 
+
+På en iphone skal man åbne siden i Safari, trykke på Action ikonet og vælge "Tilføj til hjemmeskærm".
+
+### Script
+___
+1. Start med at lave noget UI til dit banner - eksempelvis under footeren  på din *index.html* side. 
+```html
+<section id="a2hs-banner">
+    <h2>Tilføj til hjemmeskærm</h2>
+    <div class="grid">
+      <div class="div1">
+        <img alt="PWA Startup Ikon" src="/assets/images/icons/favicon-32x32.png" />
+      </div>
+      <div class="div2">
+        <strong>PWA Start</strong><br>
+        Min første App
+      </div>
+      <div class="div3">
+        <button type="button" id="btnadd">Ok</button>
+        <button type="button" id="btndismiss">Cancel</button>
+      </div>
+    </div>
+</section>
+```
+___
+2. Banneret er nu altid synligt på dit site. Start med at sætte en *display:none* på banneret i dit CSS.
+___
+3. Opret en javascript fil i din *js* mappe. Kald den eventuelt for *add2homescreen.js*.
+___
+3. I javascript filen skal vi først sætte en variabel til vores prompt
+```javascript
+
+```
+___
+
+## Reflektion
+Prøv eventuelt at gennemgå guiden hvor du  øver dig i at forklare de enkelte trin for dig selv eller en klassekammerat/ven. 
+
+Skriv eventuelt ned hvilke dele kan du forstår, kan forklare og anvende. 
+
+Skriv også ned hvis der er ord eller begreber som du ikke forstår.
--- public_html/assets/js/add2homescreen.js
+++ public_html/assets/js/add2homescreen.js
@@ -5,6 +5,7 @@
 
 // Sætter variabler
 let deferredPrompt;
+let a2hsBanner = document.getElementById('a2hs-banner');
 
 /**
  * Hvis PWA kriterier for tilføjelse til startskærm er opfyldt, 
@@ -17,60 +18,35 @@ window.addEventListener('beforeinstallprompt', (e) => {
     console.log('Før installation af prompt');
     // Gem event så vi kan kalde det senere
     deferredPrompt = e;
-    // Opdater UI - kald funktion der giver brugeren 
-    // en besked om at de kan tilføje til hjemmeskærm
-    showInstallPromotion();
-});
+    // Opdater UI sæt banner til synlig
+    a2hsBanner.style.display = 'block';
+ });
 
-/**
+ /**
  * Funktion til besked
  */
-function showInstallPromotion() {
-  // Bygger UI til besked
-  let htmlprompt = `
-    <section class="installprompt">
-      <h2>Tilføj til hjemmeskærm</h2>
-      <div class="grid">
-        <div class="div1">
-          <img alt="PWA Startup Ikon" src="/assets/images/icons/favicon-32x32.png" />
-        </div>
-        <div class="div2">
-          <strong>PWA Start</strong><br>
-          Min første App :)
-        </div>
-        <div class="div3">
-          <button type="button" id="btnadd">Ok</button>
-          <button type="button" id="btndismiss">Cancel</button>
-        </div>
-      </div>
-    </section>
-  `;
-  // Indsætter UI på siden
-  document.getElementById('a2hs-placeholder').innerHTML = htmlprompt;
 
-  // Lytter på et event fra OK knap
-  document.getElementById('btnadd').addEventListener('click', (e) => {
-    // Skjul UI når bruger klikker
-    document.getElementById('a2hs-placeholder').style.display = 'none';
-    // Vis browser prompt
-    deferredPrompt.prompt();
-    // Vent til at bruger besvarer prompten
-    deferredPrompt.userChoice
-      .then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('Bruger har accepteret A2HS prompten');
-        } else {
-          console.log('Bruger har afvist A2HS prompten');
-        }
-        deferredPrompt = null;
-      });
-  }); 
-  
-  // Lytter på et event fra Cancel knap
-  document.getElementById('btndismiss').addEventListener('click', (e) => {
-    // Skjul UI når bruger klikker
-    document.getElementById('a2hs-placeholder').style.display = 'none';
-  });
-  
-}
+// Lytter på et event fra OK knap
+document.getElementById('btnadd').addEventListener('click', (e) => {
+  // Skjul UI når bruger klikker
+  a2hsBanner.style.display = 'none';
+  // Vis browser prompt
+  deferredPrompt.prompt();
+  // Vent til at bruger besvarer prompten
+  deferredPrompt.userChoice 
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('Bruger har accepteret A2HS prompten');
+      } else {
+        console.log('Bruger har afvist A2HS prompten');
+      }
+      deferredPrompt = null;
+    });
+});
+
+// Lytter på et event fra Cancel knap
+document.getElementById('btndismiss').addEventListener('click', (e) => {
+  // Skjul UI når bruger klikker
+  a2hsBanner.style.display = 'none';
+});
 

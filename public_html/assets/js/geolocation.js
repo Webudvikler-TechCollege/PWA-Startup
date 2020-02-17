06a1b7000b87dbebfbcb880dbb2397472378c123
Heinz K <heka@techcollege.dk>
2020-02-17 22:27:10 +0100

    Updates

--- public_html/assets/js/geolocation.js
+++ public_html/assets/js/geolocation.js
@@ -1,7 +1,7 @@
 // Hent browser language
 console.log(`Sprog: ${navigator.language}`);
 
-// Hent geo location
+// Hent geolocations latitude og longitude
 navigator.geolocation.getCurrentPosition((position) => {
     console.log(`Latitude: ${position.coords.latitude}`);
     console.log(`Longtitude: ${position.coords.longitude}`);
